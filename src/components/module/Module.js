813ad8006b414c388f381baae1a9810ed2d1337c
Pedro Henrique Gardim Miranda <me@pedrogardim.com>
2021-05-10 19:24:56 +0200

    Module expand bar

--- src/components/module/Module.js
+++ src/components/module/Module.js
@@ -8,13 +8,12 @@ import MelodyGrid from "../modules/MelodyGrid";
 import InstrumentEditor from "../InstrumentEditor/InstrumentEditor";
 import ModuleSettings from "./ModuleSettings";
 
-
 import "./Module.css";
 
 function Module(props) {
+  const [expanded, setExpanded] = useState(false);
   const [instrumentEditorMode, setInstrumentEditorMode] = useState(false);
   const [settingsMode, setSettingsMode] = useState(false);
-
 
   //const getValue = (event) => console.log(event.target.value);
 
@@ -23,20 +22,20 @@ function Module(props) {
   const handleInstrumentButtonMode = () => {
     setInstrumentEditorMode((prev) => (prev ? false : true));
     setSettingsMode(false);
-
   };
 
   const handleSettingsButtonMode = () => {
     setSettingsMode((prev) => (prev ? false : true));
     setInstrumentEditorMode(false);
-
   };
 
   switch (props.module.type) {
     case 0:
       innerModule = (
         <Sequencer
-          style={{ display: instrumentEditorMode || settingsMode ? "none" : "flex" }}
+          style={{
+            display: instrumentEditorMode || settingsMode ? "none" : "flex",
+          }}
           module={props.module}
           kit={0}
           updateModules={props.updateModules}
@@ -46,7 +45,9 @@ function Module(props) {
     case 1:
       innerModule = (
         <MelodyGrid
-          style={{ display: instrumentEditorMode || settingsMode ? "none" : "flex" }}
+          style={{
+            display: instrumentEditorMode || settingsMode ? "none" : "flex",
+          }}
           module={props.module}
           updateModules={props.updateModules}
         />
@@ -55,32 +56,40 @@ function Module(props) {
     case 2:
       innerModule = (
         <ChordProgression
-          style={{ display: instrumentEditorMode || settingsMode ? "none" : "flex" }}
+          style={{
+            display: instrumentEditorMode || settingsMode ? "none" : "flex",
+          }}
           module={props.module}
           updateModules={props.updateModules}
           setDrawer={props.setDrawer}
           drawerCont={props.drawerCont}
-
         />
       );
       break;
   }
 
   return (
-    <Card style={{backgroundColor:props.module.color[700]}} className="module">
+    <Card
+      style={{
+        backgroundColor: props.module.color[700],
+        height: expanded ? "450px" : "300px",
+        width: expanded ? "600px" : "400px",
+      }}
+      className="module"
+    >
       <div className="module-header">
         <span className="module-title">{props.module.name}</span>
-        <IconButton
-          className="module-instrument-icon-button"
-          onClick={handleInstrumentButtonMode}
-        >
-          <Icon style={{ color: "white" }}>piano</Icon>
-        </IconButton>
         <IconButton
           className="module-instrument-icon-button"
           onClick={handleSettingsButtonMode}
         >
           <Icon style={{ color: "white" }}>settings</Icon>
+        </IconButton>
+        <IconButton
+          className="module-instrument-icon-button"
+          onClick={handleInstrumentButtonMode}
+        >
+          <Icon style={{ color: "white" }}>piano</Icon>
         </IconButton>
       </div>
 
@@ -95,10 +104,21 @@ function Module(props) {
         <ModuleSettings
           module={props.module}
           updateModules={props.updateModules}
+          setSettingsMode={setSettingsMode}
           index={props.index}
         />
       )}
+
       {innerModule}
+
+      <div
+        className="expand-bar"
+        onClick={() => setExpanded(expanded ? false : true)}
+      >
+        <Icon className="expand-bar-icon">
+          {expanded ? "expand_less" : "expand_more"}
+        </Icon>
+      </div>
     </Card>
   );
 }
--- src/components/module/ModuleSettings.js
+++ src/components/module/ModuleSettings.js
@@ -1,7 +1,6 @@
-import { Select,InputLabel,FormControl } from "@material-ui/core";
+import { Select, InputLabel, FormControl } from "@material-ui/core";
 
 import { adaptSequencetoSubdiv } from "../../assets/musicutils";
-
 
 import "./ModuleSettings.css";
 
@@ -11,55 +10,52 @@ function ModuleSettings(props) {
   let mainContent = "No Settings";
 
   const handleStepsSelect = (event) => {
-      let newValue = parseInt(event.target.value);
-      props.updateModules((previous) =>
-          previous.map((module, i) => {
-            if (i === props.index) {
-              let newModule = { ...module };
-              newModule.subdiv = parseInt(newValue);
-              newModule.score = newModule.score.map((array)=>adaptSequencetoSubdiv(array,newValue))
+    let newValue = parseInt(event.target.value);
+    props.updateModules((previous) =>
+      previous.map((module, i) => {
+        if (i === props.index) {
+          let newModule = { ...module };
+          newModule.subdiv = parseInt(newValue);
+          newModule.score = newModule.score.map((array) =>
+            adaptSequencetoSubdiv(array, newValue)
+          );
 
-              return newModule;
-            } else {
-              return module;
-            }
-          })
-        );
-
-
-  }
+          return newModule;
+        } else {
+          return module;
+        }
+      })
+    );
+    props.setSettingsMode(false)
+  };
 
   switch (props.module.type) {
     case 0:
       mainContent = [
         <FormControl>
-        <InputLabel id="subdivision-select-label">Steps</InputLabel>
-        <Select native labelId="subdivision" onChange={handleStepsSelect}
->
-          {subdivisionValues.map((value, index) => (
-            <option key={index} value={value}>
-              {value}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-       ,
+          <InputLabel id="subdivision-select-label">Steps</InputLabel>
+          <Select native labelId="subdivision" onChange={handleStepsSelect}>
+            {subdivisionValues.map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </Select>
+        </FormControl>,
       ];
       break;
-      case 1:
+    case 1:
       mainContent = [
         <FormControl>
-        <InputLabel id="subdivision-select-label">Steps</InputLabel>
-        <Select native labelId="subdivision" onChange={handleStepsSelect}
->
-          {subdivisionValues.map((value, index) => (
-            <option key={index} value={value}>
-              {value}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-       ,
+          <InputLabel id="subdivision-select-label">Steps</InputLabel>
+          <Select native labelId="subdivision" onChange={handleStepsSelect}>
+            {subdivisionValues.map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </Select>
+        </FormControl>,
       ];
       break;
     case 2:
