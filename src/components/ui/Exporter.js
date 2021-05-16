05d72d68cb7e49dfd5c960e8c48addf5dd8e3847
Pedro Henrique Gardim Miranda <me@pedrogardim.com>
2021-05-16 16:24:32 +0200

    Export btn to Fab

--- src/components/ui/Exporter.js
+++ src/components/ui/Exporter.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 
-import { Button } from "@material-ui/core";
+import { Fab, Icon } from "@material-ui/core";
 
-import {bounceSessionExport } from "../../utils/exportUtils";
+import { bounceSessionExport } from "../../utils/exportUtils";
 
 function Exporter(props) {
-  const [isReady,setIsReady] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
   const modules = props.modules;
-  const sessionData = props.sessionData
+  const sessionData = props.sessionData;
 
   const handleButtonClick = () => {
     setIsReady(false);
-    bounceSessionExport(modules, sessionData, setIsReady,props.sessionSize);
+    bounceSessionExport(modules, sessionData, setIsReady, props.sessionSize);
   };
 
   return (
     <div className="exporter">
-    <Button color="primary" onClick={handleButtonClick}>Export</Button>
+      <Fab className="fixed-fab" style={{right:96}} color="primary" onClick={handleButtonClick}>
+        <Icon>file_download</Icon>
+      </Fab>
     </div>
   );
 }
