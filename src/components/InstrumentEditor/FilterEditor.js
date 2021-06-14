ed9873d6223a69f7b97f2cb29e3e1c4d45ce974f
Pedro Henrique Gardim Miranda <me@pedrogardim.com>
2021-06-14 15:15:48 +0200

    Filter editor modified (to sliders)

--- src/components/InstrumentEditor/FilterEditor.js
+++ src/components/InstrumentEditor/FilterEditor.js
@@ -9,57 +9,37 @@ import { Typography, Slider, IconButton, Icon } from "@material-ui/core";
 import "./FilterEditor.css";
 
 function FilterEditor(props) {
-  const XYSelector = useRef(null);
   //temp
   const [filterState, setFilterState] = useState(
     !(
       props.instrument.get().filter.frequency === 20001 &&
-      props.instrument.get().filter.Q === 1000 &&
       props.instrument.get().filter.type === "notch"
     )
   );
 
-  const [selecting, setIsSelecting] = useState(false);
   const [tempFilter, setTempFilter] = useState(() => {
     return new Tone.Filter({
       ...props.instrument.get().filter,
       frequency: props.instrument.get().filterEnvelope.baseFrequency,
     });
   });
-  const [selectorPos, setSelectorPos] = useState({
-    x: tempFilter.get().frequency / 20000,
-    y: tempFilter.get().Q / 5,
-  });
+  const [frequency, setFrequency] = useState(tempFilter.get().frequency);
+  const [filterQ, setFilterQ] = useState(tempFilter.get().frequency);
+
   const [filterFR, setFilterFR] = useState(tempFilter.getFrequencyResponse(64));
   const [filterFRWave, setFilterFRWave] = useState(drawWave(filterFR));
 
-  const handleXYSelect = (event) => {
-    if (selecting) {
-      let parentOffset = XYSelector.current.getBoundingClientRect();
-      let values = {
-        x: (event.clientX - parentOffset.x) / 128,
-        y: (1 - (event.clientY - parentOffset.y) / 64).toFixed(2),
-      };
-
-      values.x = values.x < 0 ? 0 : values.x > 1 ? 1 : values.x;
-      values.y = values.y < 0 ? 0 : values.y > 1 ? 1 : values.y;
-
-      setSelectorPos(values);
-
-      tempFilter.set({
-        frequency: Math.floor(
-          mapLogScale(selectorPos.x, 0, 1, Math.log(20), Math.log(20000))
-        ),
-      });
-      tempFilter.set({ Q: selectorPos.y * 5 });
-
-      setFilterFR(tempFilter.getFrequencyResponse(64));
-    }
+  const handleParameterChange = (parameter, value) => {
+    tempFilter.set({
+      [parameter]: value,
+    });
+    parameter === "frequency" ? setFrequency(value) : setFilterQ(value);
+    setFilterFR(tempFilter.getFrequencyResponse(64));
   };
 
   const registerToSynth = () => {
-    setIsSelecting(false);
-    props.handleFilterChange(tempFilter.get());
+    let filterOptions = props.handleFilterChange(tempFilter.get());
+    //setFilterFR(tempFilter.getFrequencyResponse(64));
 
     //tempFilter.dispose();
     //setTempFilter({});
@@ -71,7 +51,6 @@ function FilterEditor(props) {
       ? props.handleFilterChange({
           type: "notch",
           frequency: "20001",
-          Q: 1000,
         })
       : props.handleFilterChange(tempFilter.get());
 
@@ -79,17 +58,12 @@ function FilterEditor(props) {
   };
 
   useEffect(() => {
-    setSelectorPos({
-      x: mapLogScale(
-        tempFilter.get().frequency,
-        Math.log(20),
-        Math.log(20000),
-        1,
-        0
-      ),
-      y: tempFilter.get().Q / 5,
-      final: true,
-    });
+    setFilterState(
+      !(
+        tempFilter.get().frequency === 20001 &&
+        tempFilter.get().type === "notch"
+      )
+    );
   }, [tempFilter]);
 
   useEffect(() => {
@@ -103,14 +77,12 @@ function FilterEditor(props) {
         frequency: props.instrument.get().filterEnvelope.baseFrequency,
       })
     );
-    console.log("instr change triggered");
   }, [props.instrument]);
 
   return (
     <div className="filter-editor">
       <Typography variant="overline">Filter</Typography>
       <div className="break" />
-
       <IconButton
         color={filterState ? "primary" : "default"}
         onClick={toggleFilter}
@@ -121,43 +93,37 @@ function FilterEditor(props) {
         <Icon>power_settings_new</Icon>
       </IconButton>
       <svg
-        onMouseDown={() => setIsSelecting(true)}
-        onMouseMove={handleXYSelect}
-        onMouseUp={registerToSynth}
         width="128px"
         height="64px"
         style={{ border: "1px solid #05386b", marginBottom: 8 }}
-        ref={XYSelector}
       >
-        {filterState && (
-          <Fragment>
-            <path d={filterFRWave} stroke="#05386b" fill="none" />
-            <rect
-              x={Math.floor(selectorPos.x * 128)}
-              height="100%"
-              width="1"
-              fill="#05386b"
-              fillOpacity="0.7"
-            />
-            <rect
-              y={Math.floor((1 - selectorPos.y) * 64)}
-              height="1"
-              width="100%"
-              fill="#05386b"
-              fillOpacity="0.7"
-            />
-          </Fragment>
-        )}
+        {filterState && <path d={filterFRWave} stroke="#05386b" fill="none" />}
       </svg>
       <Typography variant="overline" className="filter-editor-labels">
-        {"Frequency:" + Math.floor(tempFilter.get().frequency) + "Hz"}
+        {"Frequency:" + Math.floor(frequency) + "Hz"}
       </Typography>
+      <Slider
+        valueLabelDisplay="auto"
+        min={20}
+        max={20000}
+        step={1}
+        value={frequency}
+        onChange={(e, v) => handleParameterChange("frequency", v)}
+        onChangeCommitted={registerToSynth}
+      />
       <div className="break" />
       <Typography variant="overline" className="filter-editor-labels">
-        {"Resonance:" + (selectorPos.y * 5).toFixed(2)}
+        {"Resonance:" + filterQ}
       </Typography>
-
-      <Slider></Slider>
+      <Slider
+        valueLabelDisplay="auto"
+        min={0}
+        max={5}
+        step={0.01}
+        value={filterQ}
+        onChange={(e, v) => handleParameterChange("Q", v)}
+        onChangeCommitted={registerToSynth}
+      />
     </div>
   );
 }
