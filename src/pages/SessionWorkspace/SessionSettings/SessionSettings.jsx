e7731238d340448f2504610b5dbbd7fdeeab5ded
Pedro Gardim <me@pedrogardim.com>
2023-10-16 16:41:29 +0200

    clean: SessionSettings modal reduced to title

--- src/pages/SessionWorkspace/SessionSettings/SessionSettings.jsx
+++ src/pages/SessionWorkspace/SessionSettings/SessionSettings.jsx
@@ -5,23 +5,12 @@ import {
   IconButton,
   Icon,
   Dialog,
-  Typography,
-  Slider,
   Grid,
   TextField,
-  Checkbox,
-  FormControlLabel,
-  Tooltip,
-  Select,
-  Avatar,
-  Autocomplete,
+  Button,
 } from "@mui/material";
 
 import { useTranslation } from "react-i18next";
-
-import { scales, musicalNotes } from "../../../services/Music";
-
-import { sessionTags } from "../../../services/MiscData";
 
 import AddUser from "../../../components/dialogs/AddUser";
 
@@ -54,234 +43,29 @@ function SessionSettings(props) {
       {
         <Dialog
           fullWidth
-          maxWidth="md"
+          maxWidth="xs"
           open={props.open}
           onClose={props.onClose}
           PaperProps={{ className: "session-settings-cont" }}
         >
           <Grid container direction="row" wrap="wrap" spacing={2}>
-            <Grid
-              container
-              item
-              direction="row"
-              alignItems="stretch"
-              sm={6}
-              spacing={2}
-            >
-              <Grid item sm={12} md={12} xl={12} lg={12} xs={12}>
-                <TextField
-                  variant="standard"
-                  style={{ width: "100%" }}
-                  value={newSessionData.name}
-                  onChange={(e) =>
-                    handleInfoChange("name", e.target.value.slice(0, 63))
-                  }
-                  label={t("info.name")}
-                />
-              </Grid>
-              <Grid item sm={12} md={12} xl={12} lg={12} xs={12}>
-                <TextField
-                  variant="standard"
-                  style={{ width: "100%" }}
-                  value={newSessionData.description}
-                  rows={2}
-                  label={t("info.description")}
-                  onChange={(e) =>
-                    handleInfoChange(
-                      "description",
-                      e.target.value.slice(0, 255)
-                    )
-                  }
-                  multiline
-                />
-              </Grid>
-              <Grid item sm={12} md={12} xl={12} lg={12} xs={12}>
-                <Autocomplete
-                  multiple
-                  className={""}
-                  options={Array(129)
-                    .fill()
-                    .map((e, i) => (e = i))}
-                  renderInput={(params) => (
-                    <TextField {...params} variant="standard" label={"Tags"} />
-                  )}
-                  onChange={(e, v) => handleInfoChange("tags", v)}
-                  value={newSessionData.tags}
-                  getOptionLabel={(e) => sessionTags[e]}
-                />
-              </Grid>
-
-              <Grid
-                className="session-settings-tabs-cont"
-                item
-                sm={12}
-                md={12}
-                xl={12}
-                lg={12}
-                xs={12}
+            <Grid item sm={12} md={12} xl={12} lg={12} xs={12}>
+              <TextField
+                variant="standard"
+                style={{ width: "100%" }}
+                value={newSessionData.name}
+                onChange={(e) =>
+                  handleInfoChange("name", e.target.value.slice(0, 63))
+                }
+                label={t("info.name")}
+              />
+              <Button
+                onClick={props.onClose}
+                variant="contained"
+                sx={{ mt: 2 }}
               >
-                {/* <Tooltip
-                  title={
-                    !props.premiumMode
-                      ? "Upgrade to Premium to use this feature"
-                      : "Session won't show up on explore"
-                  }
-                  interactive={!props.premiumMode}
-                  placement="top-start"
-                > */}
-                <FormControlLabel
-                  className="session-settings-checkbox-lbl"
-                  control={
-                    <Checkbox
-                      color="primary"
-                      checked={newSessionData.hid}
-                      /* disabled={!props.premiumMode} */
-                      disabled={true}
-                      onChange={(e) =>
-                        handleInfoChange("hid", e.target.checked)
-                      }
-                    />
-                  }
-                  label={
-                    t("workspace.options.hiddenSession") +
-                    " " +
-                    t("misc.comingSoon")
-                  }
-                  labelPlacement="end"
-                />
-                {/* </Tooltip>
-                <Tooltip
-                  title={
-                    !props.premiumMode
-                      ? "Upgrade to Premium to use this feature"
-                      : "Users won't be able to copy this session"
-                  }
-                  interactive={!props.premiumMode}
-                  placement="top-start"
-                > */}
-                <FormControlLabel
-                  className="session-settings-checkbox-lbl"
-                  control={
-                    <Checkbox
-                      color="primary"
-                      checked={newSessionData.alwcp}
-                      /* disabled={!props.premiumMode} */
-                      disabled={true}
-                      onChange={(e) =>
-                        handleInfoChange("alwcp", e.target.checked)
-                      }
-                    />
-                  }
-                  label={
-                    t("workspace.options.allowCopies") +
-                    " " +
-                    t("misc.comingSoon")
-                  }
-                  labelPlacement="end"
-                />
-                {/* </Tooltip>
-                <Tooltip
-                  title={
-                    !props.premiumMode
-                      ? "Upgrade to Premium to use this feature"
-                      : "Edit this session in real-time with other users"
-                  }
-                  interactive={!props.premiumMode}
-                  placement="top-start"
-                > */}
-                <FormControlLabel
-                  className="session-settings-checkbox-lbl"
-                  control={
-                    <Checkbox
-                      color="primary"
-                      checked={newSessionData.rte}
-                      /* disabled={!props.premiumMode} */
-                      disabled={true}
-                      onChange={(e) =>
-                        handleInfoChange("rte", e.target.checked)
-                      }
-                    />
-                  }
-                  label={
-                    t("workspace.options.realtimeEdit") +
-                    " " +
-                    t("misc.comingSoon")
-                  }
-                  labelPlacement="end"
-                />
-                {/* </Tooltip> */}
-              </Grid>
-            </Grid>
-
-            <Grid container item direction="column" sm={6}>
-              <Grid item>
-                <Typography variant="overline">
-                  {t("track.settings.sessionScale")}
-                </Typography>
-                <div className="break" />
-
-                <Select
-                  variant="standard"
-                  native
-                  defaultValue={newSessionData.root}
-                  onChange={(e) => handleInfoChange("root", e.target.value)}
-                >
-                  {musicalNotes.map((note, noteIndex) => (
-                    <option key={noteIndex} value={noteIndex}>
-                      {note}
-                    </option>
-                  ))}
-                </Select>
-                <Select
-                  variant="standard"
-                  native
-                  defaultValue={newSessionData.scale}
-                  onChange={(e) => handleInfoChange("scale", e.target.value)}
-                >
-                  {scales.map((scale, scaleIndex) => (
-                    <option key={scaleIndex} value={scaleIndex}>
-                      {t(`music.scales.${scaleIndex}`)}
-                    </option>
-                  ))}
-                </Select>
-              </Grid>
-              <div className="break" style={{ height: 16 }} />
-
-              <Grid item>
-                <Typography variant="overline">{t("Editors")}</Typography>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  {props.editorProfiles !== null &&
-                  Object.values(props.editorProfiles).length > 0 ? (
-                    Object.values(props.editorProfiles).map(
-                      (e) =>
-                        e && (
-                          <Tooltip title={e.profile.username}>
-                            <Avatar
-                              src={e.profile.photoURL}
-                              alt={e.profile.username}
-                              style={{ marginRight: 8 }}
-                              onClick={(ev) =>
-                                props.handlePageNav(
-                                  "user",
-                                  e.profile.username,
-                                  ev
-                                )
-                              }
-                            />
-                          </Tooltip>
-                        )
-                    )
-                  ) : (
-                    <Avatar />
-                  )}
-                  <IconButton
-                    style={{ height: 40, width: 40 }}
-                    onClick={() => setUserDialog(true)}
-                  >
-                    <Icon>add</Icon>
-                  </IconButton>
-                </div>
-              </Grid>
+                Save
+              </Button>
             </Grid>
           </Grid>
           <IconButton
